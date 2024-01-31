66dbbd9ac962721b2e5fb762778ef322cc287bae
NarnahEG <esthergbadamosi1@gmail.com>
2024-01-31 09:21:59 +0100

    Tested adding employee

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,13 +1,19 @@
 const { expect } = require('@wdio/globals')
 const LoginPage = require('../pageobjects/login.page')
+const AddEmployeePage = require('../pageobjects/employee.page')
+const employeePage = require('../pageobjects/employee.page')
 //const SecurePage = require('../pageobjects/secure.page')
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('My OrangeHRM Application', () => {
+    it('Login with valid credentials', async () => {
         await LoginPage.open()
-
         await LoginPage.login('Admin', 'admin123')
         expect(browser).toHaveValue('dashboard');
+    })
+
+    it('Create Employee', async () => {
+        await AddEmployeePage.open()
+        await AddEmployeePage.create('Jane', 'Aisha', 'Ashley')
     })
 })
 
